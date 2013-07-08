d0227e56fce17891c9b56486ca5ced511a97b2fd
Daniel Waardal <daniel@massforstroel.se>
2013-07-09 01:07:33 +0200

    added working_dir to settings

--- undead.py
+++ undead.py
@@ -15,6 +15,7 @@ class Undead(object):
 
     name = None
     pid = None
+    working_dir = "/"
     log_level = "WARNING"
     log_handler = None
 
@@ -36,6 +37,31 @@ class Undead(object):
         from lockfile import FileLock
         from logbook import Logger, FileHandler
         
+        process_id = os.fork()
+        if process_id < 0:
+            sys.exit(1)
+        elif process_id is not 0:
+            sys.exit(0)
+        process_id = os.setsid()
+        if process_id is -1:
+            sys.exit(1)
+        devnull = "/dev/null"
+        if hasattr(os, "devnull"):
+            devnull = os.devnull
+
+        for fd in range(getrlimit(RLIMIT_NOFILE)[0]):
+            try:
+                os.close(fd)
+            except OSError:
+                pass
+
+        os.open(devnull, os.O_RDWR)
+        os.dup(0)
+        os.dup(0)
+
+        os.umask(0o27)
+        os.chdir(self.working_dir)
+
         self.action = action
         self.name = self.name or action.__name__
 
@@ -46,62 +72,36 @@ class Undead(object):
             if not os.path.exists(home):
                 os.makedirs(home)
             self.pid = os.path.join(home, "{0}.pid".format(self.name))
+
+        self.lock = FileLock(self.pid)
+        if self.lock.is_locked():
+            sys.stderr.write("Error: {0} is locked.\n".format(self.pid))
+            sys.exit(0)
+        with open(self.pid, "w") as lockfile:
+            lockfile.write("{0}".format(os.getpid()))
+        self.lock.acquire()
         
-            process_id = os.fork()
-            if process_id < 0:
-                sys.exit(1)
-            elif process_id is not 0:
-                sys.exit(0)
-            process_id = os.setsid()
-            if process_id is -1:
-                sys.exit(1)
-            devnull = "/dev/null"
-            if hasattr(os, "devnull"):
-                devnull = os.devnull
+        # Initialize logging.
+        self.log = Logger(self.name)
 
-            for fd in range(getrlimit(RLIMIT_NOFILE)[0]):
-                try:
-                    os.close(fd)
-                except OSError:
-                    pass
-
-            os.open(devnull, os.O_RDWR)
-            os.dup(0)
-            os.dup(0)
-
-            os.umask(0o27)
-            os.chdir("/")
-
-            self.lock = FileLock(self.pid)
-            if self.lock.is_locked():
-                sys.stderr.write("Error: {0} is locked.\n".format(self.pid))
-                sys.exit(0)
-            with open(self.pid, "w") as lockfile:
-                lockfile.write("{0}".format(os.getpid()))
-            self.lock.acquire()
-            
-            # Initialize logging.
-            self.log = Logger(self.name)
-
-            if self.log_handler is None:
-                if not os.path.exists(home):
-                        os.makedirs(home)
+        if self.log_handler is None:
+            if not os.path.exists(home):
+                    os.makedirs(home)
             self.log_handler = FileHandler(
-                os.path.join(home, "{0}.log".format(self.name)),
-                level=self.log_level
-                )
-            with self.log_handler.applicationbound():
-            
-                # Set custom action on SIGTERM.
-                signal.signal(signal.SIGTERM, self._sigterm)
-                atexit.register(self._sigterm)
+                os.path.join(home, "{0}.log".format(self.name)))
+        self.log_handler.level_name = self.log_level
+        with self.log_handler.applicationbound():
 
-                self.log.warning("Starting daemon.")
+            # Set custom action on SIGTERM.
+            signal.signal(signal.SIGTERM, self._sigterm)
+            atexit.register(self._sigterm)
 
-                args = inspect.getargspec(self.action)[0]
-                if 'log' not in args:
-                    return self.action()
-                self.action(log=self.log)
+            self.log.warning("Starting daemon.")
+
+            args = inspect.getargspec(self.action)[0]
+            if 'log' not in args:
+                return self.action()
+            self.action(log=self.log)
 
     def _sigterm(self, signum=None, frame=None):
         import os

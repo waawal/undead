1bd6d23010292ccfd5267d2c1f8276e1074680b4
Daniel Waardal <daniel@massforstroel.se>
2013-07-11 12:56:00 +0200

    using a dict with defaults for daemon

--- undead.py
+++ undead.py
@@ -11,88 +11,108 @@ Dead Easy UNIX Daemons!
 
 class Undead(object):
     """ This is the Undead module """
-    import logbook
+    
+
+    
+
+    def __init__(self, name=None, log_level='WARNING',
+                 log_handler=None, **kwargs):
+
+        self.settings = {
+            'chroot_directory': None,
+            'working_directory': u'/',
+            'umask': 0,
+            'uid': None,
+            'gid': None,
+            'prevent_core': True,
+            'detach_process': None,
+            'files_preserve': None,
+            'pidfile': None,
+            'stdin': None,
+            'stdout': None,
+            'stderr': None,
+            'signal_map': None,
+        }
+        self.settings.update(kwargs)
+
+        self.name = name
+        self.log_level = log_level
+        self.log_handler = log_handler
 
 
-    def __init__(self):
-        self.name = None
-        self.log_level = "WARNING" # make a property, accept strings and numbers
-        self.log_handler = None # Check for basestring
+        def __getattr__(self, name):
+            if name in self.settings:
+                return self.settings[name]
+            else:
+                raise AttributeError
+        self.__getattr__ = __getattr__
 
-        class Settings(object):
-            def __init__(self):
-                self.chroot_directory = None
-                self.working_directory = u'/'
-                self.umask = 0
-                self.uid = None
-                self.gid = None
-                self.prevent_core = True
-                self.detach_process = None
-                self.files_preserve = None
-                self.pidfile = None
-                self.stdin = None
-                self.stdout = None
-                self.stderr = None
-                self.signal_map = None
-
-        self.settings = Settings()
+        def __setattr__(self, name, value):
+            if name in self.settings:
+                self.settings[name] = value
+            else:
+                self.__dict__[name] = value
+        self.__setattr__ = __setattr__
 
     @property
     def working_dir(self):
-        return self.settings.working_directory
+        return self.settings['working_directory']
 
     @working_dir.setter
-    def my_attr(self, value):
-        self.settings.working_directory = value
+    def working(self, value):
+        self.settings['working_directory'] = value
 
     @property
     def pid(self):
-        return self.settings.pidfile
+        return self.settings['pidfile']
 
     @pid.setter
-    def my_attr(self, value):
-        self.settings.pidfile = value
+    def pid(self, value):
+        self.settings['pidfile'] = value
 
     def __call__(self, action, *args, **kwargs):
         """ Alias for start """
         self.start = self.start(action)
 
     def start(self, action):
+        import os
+        import inspect
         import daemon
+        import logbook
         from lockfile import FileLock
 
         self.name = self.name or action.__name__
         default_dir = os.path.join(os.path.expanduser("~"),
                                 ".{0}".format(self.name))
-        if self.settings.pidfile is None:
+        if self.settings['pidfile'] is None:
             if not os.path.exists(default_dir):
                 os.makedirs(default_dir)
-            self.settings.pidfile = os.path.join(default_dir, "{0}.pid".format(self.name))
+            self.settings['pidfile'] = os.path.join(default_dir, "{0}.pid".format(self.name))
 
         # Initializing daemon.
-        context = daemon.DaemonContext(**self.settings.__dict__)
+        context = daemon.DaemonContext(**self.settings)
         # Initialize logging if requested.
-        action_args = inspect.getargspec(self.action)[0]
+        action_args = inspect.getargspec(action)[0]
         if 'log' in action_args:
-            self.log = logger.Logger(self.name)
+            self.log = logbook.Logger(self.name)
 
             if self.log_handler is None:
-            if not os.path.exists(default_dir):
+                if not os.path.exists(default_dir):
                     os.makedirs(default_dir)
-            self.log_handler = logbook.FileHandler(
-                os.path.join(default_dir, "{0}.log".format(self.name)))
-            if self.settings.files_preserve is None:
-                self.settings.files_preserve = [self.log_handler.stream]
-            else:
-                self.settings.files_preserve.append(self.log_handler.stream)
+                self.log_handler = logbook.FileHandler(
+                    os.path.join(default_dir, "{0}.log".format(self.name)))
+                if self.settings['files_preserve'] is None:
+                    self.settings['files_preserve'] = [self.log_handler.stream]
+                else:
+                    self.settings['files_preserve'].append(self.log_handler.stream)
             self.log_handler.level_name = self.log_level
             with self.log_handler.applicationbound():
                 self.log.warning("Starting daemon.")
                 with context:
-                    self.action(log=self.log)
+                    action(log=self.log)
         else:
             with context:
-                self.action()
+                action()
 
 
 undead = Undead()
